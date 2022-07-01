7c91d7bcc8cb77686b327be8b3a1a3517118fa7b
Adrien Wald <adrien@genei.io>
2022-07-01 13:42:20 +0100

    demonstrate broken rendering when zooming in

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,24 @@ function App() {
         <div className='menu'>
           Menu seems to cause selection offset
         </div>
-        <div className='viewer-container'>
+        {/* <div className='viewer-container'>
           <PdfViewer
             id='3'
             url='https://arxiv.org/pdf/2009.01325.pdf'
           />
-        </div>
+        </div> */}
         <div className='viewer-container'>
           <PdfViewer
             id='1'
-            url='https://genei-cdn-resources.s3.eu-west-1.amazonaws.com/e11024aa-537d-4ca5-b0f9-01235221f6dd.pdf'
+            url='https://genei-cdn-resources.s3.eu-west-1.amazonaws.com/Can+genomics+tell+me+who+I+am+Essentialistic+rhetoric+in+direct+to+consumer+DNA+testing.pdf'
           />
         </div>
-        <div className='viewer-container'>
+        {/* <div className='viewer-container'>
           <PdfViewer
             id='2'
             url='https://genei-cdn-resources.s3.eu-west-1.amazonaws.com/Global+Burden+of+Cardiovascular+Diseases+and+Risk+Factors%2C+1990%E2%80%932019.pdf'
           />
-        </div>
+        </div> */}
       </div>
     </PDFTronProvider>
   )
